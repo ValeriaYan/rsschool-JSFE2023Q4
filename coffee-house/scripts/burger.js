21e4e007a732234cf800b8df01a02070024e6541
Valeria <valeria.yanycheva@gmail.com>
2023-12-13 05:41:48 +0600

    fix: fix burger behavior

--- coffee-house/scripts/burger.js
+++ coffee-house/scripts/burger.js
@@ -1,6 +1,8 @@
 const burgerIcon = document.querySelector('.burger-icon');
 const burger = document.querySelector('.burger');
 const burgerList = document.querySelector('.burger__nav .nav__list');
+const menuLink = document.querySelector('.header__menu');
+const logo = document.querySelector('.header__logo');
 
 burgerIcon.addEventListener('click', toggleBurger);
 burgerList.addEventListener('click', function(event) {
@@ -9,6 +11,12 @@ burgerList.addEventListener('click', function(event) {
         toggleBurger();
     }
 })
+menuLink.addEventListener('click', toggleBurger);
+logo.addEventListener('click', function() {
+    if(burger.classList.contains('active')) {
+        toggleBurger();
+    }
+});
 
 function toggleBurger() {
     burger.classList.toggle('active');
